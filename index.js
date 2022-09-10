b64e28a5f7e4ffe0235c75a572d80a3ef9445816
Myles Linder <myles.linder@gmail.com>
2022-09-10 18:36:07 -0400

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('prettier').Config}
+ */
+module.exports = {
+	useTabs: true,
+	tabWidth: 2,
+	printWidth: 80,
+	singleQuote: false,
+	printWidth: 80,
+	trailingComma: "all",
+};
